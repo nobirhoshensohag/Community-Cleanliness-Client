a9224cc939b25b1f129eee47bb33c8b8ddbf1c22
nobirhoshensohag <nobirhoshensohag708@gmail.com>
2025-11-17 17:44:29 +0600

    add issusecategori

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div>
-            loading
+        
+            <div className="flex items-center justify-center min-h-screen bg-base-200">
+      <div className="flex flex-col items-center space-y-4">
+        {/* DaisyUI spinner */}
+        <span className="loading loading-spinner loading-lg text-primary"></span>
+
+        {/* Optional text */}
+        <p className="text-lg font-semibold text-primary">Loading...</p>
+      </div>
         </div>
     );
 };
