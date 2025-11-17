e4966ca1569c4cf52c1b9447274c320c23e4b51b
nobirhoshensohag <nobirhoshensohag708@gmail.com>
2025-11-17 14:39:53 +0600

    add pages & router setup

---
+++ src/Pages/AddIssues.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddIssues = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddIssues;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import './index.css'
+import { RouterProvider } from "react-router";
+import router from "./Router/Router.jsx";
+import AuthProvider from "./Provider/AuthProvider.jsx";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthProvider>
+    
   </StrictMode>,
 )
