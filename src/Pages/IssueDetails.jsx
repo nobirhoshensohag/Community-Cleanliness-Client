698ccf52125157b1e70dfa0a9b2a7b836fc2a48f
nobirhoshensohag <nobirhoshensohag708@gmail.com>
2025-11-17 23:01:42 +0600

    implement sort

--- src/Pages/IssueDetails.jsx
+++ src/Pages/IssueDetails.jsx
@@ -19,7 +19,7 @@ const IssueDetails = () => {
   const axiosInstance = useAxios();
    const [contricbutions, setContricbutions] = useState([]);
   const [refetch, setRefetch] = useState(false);
-  console.log(user);
+  console.log(data);
 
   const handlePayUpContribution = (e) => {
     e.preventDefault();
@@ -185,7 +185,7 @@ const IssueDetails = () => {
                 <input
                   type="email"
                   name="email"
-                  defaultValue={data?.email}
+                  defaultValue={user?.email}
                   readOnly
                   className="input input-bordered w-full"
                 />
